cb3555ae29a3d23cce084dec05cde6a08b313c8a
Leonardo de Oliveira Ramos <148880946+LE0N4RDOR4M0S@users.noreply.github.com>
2024-02-04 18:10:32 -0400

    Rules

--- TorreHanoi/src/App.jsx
+++ TorreHanoi/src/App.jsx
@@ -1,48 +1,52 @@
-import { useState } from 'react'
-import './App.css'
+import { useState } from 'react';
+import './App.css';
 
 function App() {
-  const [towers, setTowers] = useState([6,0,0]);
+  const [towers, setTowers] = useState([[5,4,3,2,1],[],[]]);
+  const [selectedTowerIndex, setSelectedTowerIndex] = useState();
 
-  const [selectedTowerIndex, setSelectedTowerindex] = useState();
-
-  function handleClickedTower(clikedToweridx){
+  function handleClickedTower(clickedTowerIdx) {
     if (selectedTowerIndex !== undefined) {
-      const newTowers = [...towers]
-      newTowers[selectedTowerIndex]--;
-      newTowers[clikedToweridx]++;
-      setTowers(newTowers);
-      setSelectedTowerindex(undefined);
-    }else{
-      setSelectedTowerindex(clikedToweridx);
+      const newTowers = [...towers];
+      const poppedDisc = newTowers[selectedTowerIndex].pop();
+
+      if (newTowers[clickedTowerIdx].length === 0 || newTowers[clickedTowerIdx][newTowers[clickedTowerIdx].length - 1] > poppedDisc) {
+        newTowers[clickedTowerIdx].push(poppedDisc);
+        setTowers(newTowers);
+      }
+      setSelectedTowerIndex(undefined);
+    } else {
+      setSelectedTowerIndex(clickedTowerIdx);
     }
   }
 
-  return(
+  return (
     <div className="App">
       <div className="towers">
-        {towers.map((towerHeight, towerIndex) => (
-          <div
-          onClick={() => handleClickedTower(towerIndex)}
-          className={"tower " + (selectedTowerIndex === towerIndex ? "selected" : "")}
-          key={towerIndex}>
-            <div className="line"></div>
-            <div className="dics">
-              {[...new Array(towerHeight)].map((_, discIndex) => (
-                // eslint-disable-next-line react/jsx-key
-                <div
-                key={discIndex}
-                className="disk" style={{
-                  width: `${discIndex * 10 + 10}px`,
-                  }}>
-                </div>
-              ))}
+        {towers.map((discs, towerIndex) => {
+          const reversedDiscs = discs.slice().reverse();
+          return (
+            <div
+              onClick={() => handleClickedTower(towerIndex)}
+              className={"tower " + (selectedTowerIndex === towerIndex ? "selected" : "")}
+              key={towerIndex}
+            >
+              <div className="line"></div>
+              <div className="dics">
+                {reversedDiscs.map((discNumber, index) => (
+                  <div
+                    key={index}
+                    className="disk"
+                    style={{ width: `${discNumber * 15 + 15}px` }}
+                  ></div>
+                ))}
+              </div>
             </div>
-            </div>
-        ))}
+          );
+        })}
       </div>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
